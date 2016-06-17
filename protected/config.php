2753a7e11dbbda00249b61a7d3dc1089eb7819d7
AnnaOzer <ozerskaya.ru@mail.ru>
2016-06-17 16:00:03 +0400

    migration categories

--- protected/config.php
+++ protected/config.php
@@ -1,3 +1,15 @@
 <?php
 
-return [];
+return [
+    'db' => [
+        'default' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'user' => 'root',
+            'password' => '',
+            'dbname' => 't4testd4'
+        ]
+    ],
+    'name' => 'Тест фреймворка',
+    'extensions' => ['bootstrap' => ['theme' => 'cosmo'] ]
+];
